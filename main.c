e720f2625d43914a33b026630d194e87b98181e3
nohy6630 <nohy6630@gmail.com>
2023-11-19 18:23:56 +0900

    fix: main.c 수정

--- main.c
+++ main.c
@@ -7,21 +7,11 @@ void initialize();
 void print_ast();
 
 void main(int argc, char *argv[])
-{ // 적당히 고쳐서 사용하세요
-
-	/*	if ((yyin = fopen(argv[argc-1],"r"))==NULL){
-			printf("can not open input file: %s\n",argv[argc-1]);
-			exit(1);
-		} */
-
+{
 	initialize();
-
-	printf("syntax analysis start!\n");
 	yyparse();
-
-	if (!syntax_err)
-	{
-		printf("syntax analysis end (no error)\n");
-		print_ast(root); // "print.c" function
-	}
+	if (syntax_err)
+		exit(1);
+	print_ast(root);
+	exit(0);
 }
